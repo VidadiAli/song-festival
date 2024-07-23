95ea0c8f07368d18eb4c9c95b2839fa4c374a883
VidadiAli <vidadiali.tiva@gmail.com>
2024-07-23 21:33:00 +0400

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './reset.css'
 import { BrowserRouter } from 'react-router-dom'
-import { store } from './Redux/Store.tsx'
-import { Provider } from 'react-redux'
+// import { store } from './Redux/Store.tsx'
+// import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
     <BrowserRouter >
       <App />
     </BrowserRouter>
-  </Provider>
 )
