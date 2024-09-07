bf2be54769425323350bb2c719e8a2ad26411548
VidadiAli <vidadiali.tiva@gmail.com>
2024-09-07 18:35:47 +0400

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 // import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/song-festival" >
+  <BrowserRouter basename="/" >
     <App />
   </BrowserRouter>
 )
